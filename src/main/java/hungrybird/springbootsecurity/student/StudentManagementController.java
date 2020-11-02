03ee5226f602111c49ab0024e5a2fc59a3bbbf93
Vincent-hide <hidehiro.okayama@gmail.com>
2020-11-02 14:13:34 -0500

    METHOD_BASE_AUTHENTICATION: switched to method base authentication by using annotation

--- src/main/java/hungrybird/springbootsecurity/student/StudentManagementController.java
+++ src/main/java/hungrybird/springbootsecurity/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package hungrybird.springbootsecurity.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -14,22 +15,31 @@ public class StudentManagementController {
     new Student(3, "Suho")
   );
 
+  // hasRole('Role_')
+  // hasAnyRole('Role_')
+  // hasAuthority('permission')
+  // hasAnyAuthority('permission')
+
   @GetMapping
+  @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
   public List<Student> getAllStudents() {
     return this.STUDENTS;
   }
 
   @PostMapping
+  @PreAuthorize("hasAuthority('student:write')")
   public void registerNewStudent(@RequestBody Student student) {
     System.out.println(student);
   }
 
   @DeleteMapping(path = "{studentId}")
+  @PreAuthorize("hasAuthority('student:write')")
   public void deleteStudent(@PathVariable("studentId") Integer studentId) {
     System.out.println(studentId);
   }
 
   @PutMapping(path = "{studentId}")
+  @PreAuthorize("hasAuthority('student:write')")
   public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
     System.out.println(String.format("%s %s", studentId, student));
   }
