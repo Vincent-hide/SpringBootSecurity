ff60867fc1c3f9c3c34ed3cfb260d241a89d157d
Vincent-hide <hidehiro.okayama@gmail.com>
2020-11-02 11:20:08 -0500

    ROLE_BASE_AUTHENTICATION:allow only ROLE_STUDENTS to access api/v1/students. ADMIN cannt be authenticated to it

--- src/main/java/hungrybird/springbootsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/hungrybird/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -3,11 +3,9 @@ package hungrybird.springbootsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -29,8 +27,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
       .authorizeRequests()
-      .antMatchers("/", "index", "/css/*", "/js/*")
-      .permitAll()
+      .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+      .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
       .anyRequest()
       .authenticated()
       .and()
