a975a60dc281b921a6f03b1f7cba9907735ceba5
Vincent-hide <hidehiro.okayama@gmail.com>
2020-11-02 12:05:22 -0500

    CRUD operation added for admin user with csrf().disable() method in configure function

--- src/main/java/hungrybird/springbootsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/hungrybird/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -26,9 +26,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
+      .csrf().disable()
       .authorizeRequests()
       .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
-      .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
+      .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name()) // allows only student to access api/v1/students
       .anyRequest()
       .authenticated()
       .and()
@@ -38,18 +39,27 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   @Bean
   protected UserDetailsService userDetailsService() {
+    // ROLE: Student
     UserDetails vince = User.builder()
       .username("vince")
       .password(passwordEncoder.encode("password"))
       .roles(ApplicationUserRole.STUDENT.name()) // ROLE_STUDENT
       .build();
 
+    // Role: Admin
     UserDetails hide = User.builder()
       .username("hide")
-      .password(passwordEncoder.encode("password123"))
+      .password(passwordEncoder.encode("password"))
       .roles(ApplicationUserRole.ADMIN.name())
       .build();
 
-    return new InMemoryUserDetailsManager(vince, hide);
+    // Role: Admin Trainee
+    UserDetails suho = User.builder()
+      .username("suho")
+      .password(passwordEncoder.encode("password"))
+      .roles(ApplicationUserRole.ADMINTRAINEE.name())
+      .build();
+
+    return new InMemoryUserDetailsManager(vince, hide, suho);
   }
 }
