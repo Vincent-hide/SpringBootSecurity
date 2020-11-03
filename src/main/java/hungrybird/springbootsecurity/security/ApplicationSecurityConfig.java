ffafc4415d9159e3d3dd2da5b1bb21cd711f44fc
Vincent-hide <hidehiro.okayama@gmail.com>
2020-11-02 21:24:10 -0500

    logout btn added

--- src/main/java/hungrybird/springbootsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/hungrybird/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -48,14 +49,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         .formLogin()
         .loginPage("/login").permitAll()
         .defaultSuccessUrl("/courses", true)
+        .passwordParameter("password")
+        .usernameParameter("username")
       .and()
         .rememberMe()
   //      .tokenRepository() // it can be used when you store the session ID in database
         .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
         .key("somethingverysecured")
+        .rememberMeParameter("remember-me")
       .and()
         .logout()
         .logoutUrl("/logout")
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
         .clearAuthentication(true)
         .invalidateHttpSession(true)
         .deleteCookies("JSESSIONID", "remember-me")
